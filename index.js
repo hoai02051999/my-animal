69741c8acfafb286b987280a4017e2d7cf82f1fc
hoai viet co de gioi <tranngochoai02051999@gmail.com>
2019-08-29 14:45:07 +0700

    insert and cut cu

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var dog= require('./dog');
 var mouse=require('./mouse');
-var mouse=new mouse('chitchit','one');
+var mouse=new mouse('chitchit and cut cu','one');
 var bena= new dog('bena');
 bena.sayhi();
 console.log(mouse);
